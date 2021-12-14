e56db1764ebcd0a8a8d625542a057eb67e8a8614
ikequintel <ike.quintel@hotmail.com>
2021-12-13 22:57:14 -0300

    Prova

---
+++ src/@core/layouts/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>
+import { BLink } from 'bootstrap-vue'
+
+export default {
+  components: {
+    BLink,
+  },
+}
+</script>
---
+++ src/@core/layouts/components/app-navbar/components/DarkToggler.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<script>
+import useAppConfig from '@core/app-config/useAppConfig'
+import { computed } from '@vue/composition-api'
+import { BNavItem } from 'bootstrap-vue'
+
+export default {
+  components: {
+    BNavItem,
+  },
+  setup() {
+    const { skin } = useAppConfig()
+
+    const isDark = computed(() => skin.value === 'dark')
+
+    return { skin, isDark }
+  },
+}
+</script>
